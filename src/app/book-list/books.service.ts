886dcca639534a4adb56e63abb2de7a1c31d546b
anton-marchenko <fattan@ya.ru>
2021-10-27 10:45:38 +0300

    add mock data

--- src/app/book-list/books.service.ts
+++ src/app/book-list/books.service.ts
@@ -1,18 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { mock } from '@app/book-list/books.mock';
 import { Book } from '@app/book-list/books.model';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class GoogleBooksService {
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   getBooks(): Observable<Array<Book>> {
-    return this.http
-      .get<{ items: Book[] }>(
-        'https://www.googleapis.com/books/v1/volumes?maxResults=5&orderBy=relevance&q=oliver%20sacks'
-      )
-      .pipe(map((books) => books.items || []));
+    return of(mock).pipe(map((books) => books.items || []));
   }
 }
